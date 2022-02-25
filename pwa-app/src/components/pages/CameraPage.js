a0d7f7a5859a066502562cd9362b9da70cd9e0c2
oliviahedlund <oliviahedlund@live.se>
2022-02-25 15:22:37 +0100

    ta bild, stäng ner, ta ny bild

--- pwa-app/src/components/pages/CameraPage.js
+++ pwa-app/src/components/pages/CameraPage.js
@@ -3,24 +3,49 @@ import "../../App.css";
 import Camera from 'react-html5-camera-photo';
 /*import 'react-html5-camera-photo/build/css/index.css';*/
 import './CameraPage.css'
-import { useState, useRef } from "react";
+import { useState } from "react";
+/*import ImagePreview from "./ImagePreview";*/
 
 function CameraPage() {
+
   const [errorMessage, setErrorMessage] = useState('');
+  const [dataUri, setDataUri] = useState('');
 
   function handleCameraError (error) {
     setErrorMessage(error.toString());
+  }
+
+  function handleTakePhoto (dataUri) {
+    setDataUri(dataUri);
+  }
+
+  function closePic () {
+    setDataUri('');
   }
 
   return (
     <div id="container" className="container">
       <h1>Hello from Camera!</h1>
       <p className="error">{errorMessage}</p>
-      <Camera 
-        isSilentMode={true}
-        onCameraError={(error) => {handleCameraError(error);}}
-        isDisplayStartCameraError={false}
-      />
+      
+        {
+          (dataUri)
+          ? <div className="react-html5-camera-photo">
+              <div className="btn" >
+                <i class="far fa-times-circle" onClick={closePic}></i>
+                </div>
+              <img src={dataUri} alt="webcam"/>
+            </div>
+          : <Camera 
+              isSilentMode={true}
+              onTakePhoto = {handleTakePhoto}
+              onCameraError={(error) => {handleCameraError(error);}}
+              isDisplayStartCameraError={false}
+              isFullscreen={false}
+          />
+        }
+      
+  
     </div>
   );
 }
