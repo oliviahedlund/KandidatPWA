bd60a5379cbd3d3310aa7e472bccd01dbd41ac37
oliviahedlund <oliviahedlund@live.se>
2022-03-03 08:53:43 +0100

    Kamera med switch

--- pwa-app/src/components/pages/CameraPage.js
+++ pwa-app/src/components/pages/CameraPage.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "../../App.css";
-import Camera from 'react-html5-camera-photo';
-/*import 'react-html5-camera-photo/build/css/index.css';*/
+import Camera, {FACING_MODES} from 'react-html5-camera-photo';
 import './CameraPage.css'
 import { useState } from "react";
-/*import ImagePreview from "./ImagePreview";*/
+import { MobileView } from 'react-device-detect';
 
 function CameraPage() {
-
+  
   const [errorMessage, setErrorMessage] = useState('');
   const [dataUri, setDataUri] = useState('');
+  const [facingMode, setFacingMode] = useState(FACING_MODES.USER);
+  const [mirroredMode, setMirroredMode] = useState(true);
 
   function handleCameraError (error) {
     setErrorMessage(error.toString());
@@ -23,6 +24,12 @@ function CameraPage() {
     setDataUri('');
   }
 
+  function switchCamera () {
+    (mirroredMode) 
+    ? setMirroredMode(false) 
+    : setMirroredMode(true);
+  }
+
   return (
     <div id="container" className="container">
       <h1>Hello from Camera!</h1>
@@ -31,21 +38,27 @@ function CameraPage() {
         {
           (dataUri)
           ? <div className="react-html5-camera-photo">
-              <div className="btn" >
-                <i class="far fa-times-circle" onClick={closePic}></i>
-                </div>
+              <div className="btn">
+                  <i class="far fa-times-circle" onClick={closePic}></i>
+              </div>
               <img src={dataUri} alt="webcam"/>
             </div>
-          : <Camera 
+          : 
+          <div className="react-html5-camera-photo">
+            <MobileView className="btn" >
+              <i class="fas fa-exchange-alt" onClick={switchCamera}></i>
+            </MobileView>
+            <Camera
               isSilentMode={true}
               onTakePhoto = {handleTakePhoto}
               onCameraError={(error) => {handleCameraError(error);}}
               isDisplayStartCameraError={false}
               isFullscreen={false}
-          />
+              idealFacingMode = {(mirroredMode)? FACING_MODES.USER: FACING_MODES.ENVIRONMENT}
+              isImageMirror = {mirroredMode}
+            />
+          </div>
         }
-      
-  
     </div>
   );
 }
